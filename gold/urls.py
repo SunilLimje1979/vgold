98f46acef779446909a5308edf6cab31f5c8e991
Pawarshekhar709 <pawarshekhar708@gmail.com>
2024-05-10 12:03:15 +0530

    vold

---
+++ gold/urls.py
@@ -0,0 +1,64 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.Login, name='login'),
+    
+    path('otp/', views.OTP, name='otp'),
+    
+    path('registration/', views.Registration, name='registration'),
+    
+    path('dashboard/', views.Dashboard, name='dashboard'),
+    
+    path('loan/', views.Loan, name='loan'),
+      
+    path('add_gold/', views.Add_gold, name='add_gold'),
+    
+    path('withdraw/', views.Withdraw, name='withdraw'),
+    
+    path('sell_gold/', views.Sell_gold, name='sell_gold'),
+    
+    path('gold_plan/', views.Gold_plan, name='gold_plan'),
+    
+    path('our_vendors/', views.Our_vendors, name='our_vendors'),
+    
+    path('gold_wallet/', views.Gold_wallet, name='gold_wallet'),
+    
+    path('gbooking_history/', views.Gbooking_history, name='gbooking_history'),
+    
+    path('gold_booking/', views.Gold_booking, name='gold_booking'),
+    
+    path('booking_details/', views.Booking_details, name='booking_details'),
+    
+    path('gdeposit_history/', views.Gdeposit_history, name='gdeposit_history'),
+    
+    path('gold_deposit/', views.Gold_deposit, name='gold_deposit'),
+    
+    path('membership/', views.Membership, name='membership'),
+    
+    path('money_wallet/', views.Money_wallet, name='money_wallet'),
+    
+    path('pay_installment/', views.Pay_installment, name='pay_installment'),
+    
+    path('add_money/', views.Add_money, name='add_money'),
+    
+    path('add_bank/', views.Add_bank, name='add_bank'),
+    
+    path('channel_partner/', views.Channel_partner, name='channel_partner'),
+    
+    path('review/', views.Review, name='review'),
+        
+    path('profile/', views.Profile, name='profile'),  
+      
+    path('update_profile/', views.Update_profile, name='update_profile'),
+    
+    path('certificate/', views.Certificate, name='certificate'),
+    
+    path('complaint/', views.Complaint, name='complaint'),
+    
+    path('feedback/', views.Feedback, name='feedback'),
+    
+    path('refer/', views.Refer, name='refer'),
+
+    
+]
---
+++ gold/views.py
@@ -0,0 +1,185 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Login(request):
+    return render(request, 'gold/login.html')
+
+# Create your views here.
+def OTP(request):
+    return render(request, 'gold/otp.html')
+
+# Create your views here.
+def Registration(request):
+    return render(request, 'gold/registration.html')
+
+# Create your views here.
+def Dashboard(request):
+    return render(request, 'gold/dashboard.html')
+
+def Loan(request):
+    return render(request, 'gold/loan.html')
+
+def Withdraw(request):
+    return render(request, 'gold/withdraw.html')
+
+def Sell_gold(request):
+    return render(request, 'gold/sell_gold.html')
+
+def Add_gold(request):
+    return render(request, 'gold/add_gold.html')
+
+def Gold_plan(request):
+    return render(request, 'gold/gold_plan.html')
+
+def Our_vendors(request):
+    return render(request, 'gold/our_vendors.html')
+
+def Gold_wallet(request):
+    return render(request, 'gold/gold_wallet.html')
+
+def Profile(request):
+    return render(request, 'gold/profile.html')
+
+def Update_profile(request):
+    return render(request, 'gold/update_profile.html')
+
+def Certificate(request):
+    return render(request, 'gold/certificate.html')
+
+def Gbooking_history(request):
+    # Dummy JSON data
+    dummy_data = [
+        {
+            "number": "VG124356",
+            "status": "Active",
+            "logo_url": "{% static 'assets/img/vgold.jpg' %}",
+            "today_gain": "125",
+            "paid_amount": "13000",
+            "emi_date": "25-02-2024",
+            "booking_date": "25-02-2024",
+            "weight": "20.00 gm",
+            "rate": "123654",
+            "value": "13000",
+            "tenure": "60 Month",
+            "booking_amount": "2000.00",
+            "booking_charge": "125",
+            "balance_amount": "13000",
+            "closing_date": "25-02-2024"
+        },
+         {
+            "number": "VG124356",
+            "status": "Closed",
+            "logo_url": "{% static 'assets/img/vgold.jpg' %}",
+            "today_gain": "125",
+            "paid_amount": "13000",
+            "emi_date": "25-02-2024",
+            "booking_date": "25-02-2024",
+            "weight": "20.00 gm",
+            "rate": "123654",
+            "value": "13000",
+            "tenure": "60 Month",
+            "booking_amount": "2000.00",
+            "booking_charge": "125",
+            "balance_amount": "13000",
+            "closing_date": "25-02-2024"
+        },
+          {
+            "number": "VG124356",
+            "status": "Closed",
+            "logo_url": "{% static 'assets/img/vgold.jpg' %}",
+            "today_gain": "125",
+            "paid_amount": "13000",
+            "emi_date": "25-02-2024",
+            "booking_date": "25-02-2024",
+            "weight": "20.00 gm",
+            "rate": "123654",
+            "value": "13000",
+            "tenure": "60 Month",
+            "booking_amount": "2000.00",
+            "booking_charge": "125",
+            "balance_amount": "13000",
+            "closing_date": "25-02-2024"
+        },
+         
+          {
+            "number": "VG124356",
+            "status": "Closed",
+            "logo_url": "{% static 'assets/img/vgold.jpg' %}",
+            "today_gain": "125",
+            "paid_amount": "13000",
+            "emi_date": "25-02-2024",
+            "booking_date": "25-02-2024",
+            "weight": "20.00 gm",
+            "rate": "123654",
+            "value": "13000",
+            "tenure": "60 Month",
+            "booking_amount": "2000.00",
+            "booking_charge": "125",
+            "balance_amount": "13000",
+            "": "25-02-2024"
+        },
+        # Add more dummy data as needed
+    ]
+    return render(request, 'gold/gbooking_history.html', {'dummy_data': dummy_data})
+
+def Gold_booking(request):
+    return render(request, 'gold/gold_booking.html')
+
+def Booking_details(request):
+    
+    return render(request, 'gold/booking_details.html')
+
+def Gdeposit_history(request):
+    # Dummy JSON data
+    dummy_data = [
+         {
+            "number": "VG124356",
+            "status": "Active",
+            "booking_date": "January 1, 2024",  # Gold Deposit Date
+            "weight": "50 grams",  # Gold Deposit Weight
+            "booking_charge": "$50",  # Processing Fees
+            "rate": "99.9%",  # Gold Purity
+            "tenure": "12 months",  # Tenure
+            "remark": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",  # Remark
+            "closing_date": "December 31, 2024",  # Gold Maturity Date
+            "gold_maturity_weight": "50 grams",  # Gold Maturity Weight
+            "todays_deposit_weight": "5 grams",  # Today's Deposit Weight
+            "todays_deposit_value": "$500"  # Today's Deposit Value
+        },
+        # Add more dummy data as needed
+    ]
+    return render(request, 'gold/gdeposit_history.html', {'dummy_data': dummy_data})
+
+def Gold_deposit(request):
+    return render(request, 'gold/gold_deposit.html')
+
+def Membership(request):
+    return render(request, 'gold/membership.html')
+
+def Money_wallet(request):
+    return render(request, 'gold/money_wallet.html')
+
+def Pay_installment(request):
+    return render(request, 'gold/pay_installment.html')
+
+def Add_money(request):
+    return render(request, 'gold/add_money.html')
+
+def Add_bank(request):
+    return render(request, 'gold/add_bank.html')
+
+def Channel_partner(request):
+    return render(request, 'gold/channel_partner.html')
+
+def Review(request):
+    return render(request, 'gold/review.html')
+
+def Complaint(request):
+    return render(request, 'gold/complaint.html')
+
+def Feedback(request):
+    return render(request, 'gold/feedback.html')
+
+def Refer(request):
+    return render(request, 'gold/refer.html')
+
